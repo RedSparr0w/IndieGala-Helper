86383463600b87ff1dc94e0a2d10b964e8707eb2
RedSparr0w <dan-jmn@hotmail.com>
2016-06-01 18:19:05 +1200

    add support for giveaways page

--- indiegala.js
+++ indiegala.js
@@ -13,6 +13,7 @@ function notifyMe(body,title="IndieGala Helper",icon="https://www.indiegala.com/
 		});
 	}
 }
+
 var myvar = '<link href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">'+
 '<div id="indiegala-helper">'+
 '	<h2>IndieGala Helper </h2><br>'+
@@ -33,29 +34,14 @@ var myvar = '<link href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/
 '		<input type="submit" id="refreshOwned" class="palette-background-2" value="Refresh Owned Games">'+
 '	</div>'+
 '</div>';
-function showOwnedGames(){
-	var apps = JSON.parse(localStorage.getItem("ownedApps"));
-	$.each(bundleApps,function(i,v){
-		if(typeof apps[v] != "undefined"){
-			$("[src$='"+v+".jpg']").parents(".bundle-item-cont").parent().addClass("owned");
-		}
-	});
-}
-$('.bundle_page').before(myvar);
-steamid=false;
-if(localStorage.getItem("SteamID") != null && localStorage.getItem("SteamID").length >=1){
-	$("#SteamID").val(localStorage.getItem("SteamID"));
-	steamid=true;
-}
+
+$('.header-placeholder').after(myvar);
+
 $('#indiegala-helper h2').click(function(e){
 	$('#indiegala-helper h2').toggleClass("open");
 	$('#indiegala-helper .input-wrapper').slideToggle(250);
 });
-steamid=false;
-if(localStorage.getItem("SteamID") != null && localStorage.getItem("SteamID").length >=1){
-	$("#SteamID").val(localStorage.getItem("SteamID"));
-	steamid=true;
-}
+
 $('#saveDetails').click(function(e){
 	e.preventDefault();
 	localStorage.setItem("APIKey", $("#APIKey").val());
@@ -63,36 +49,28 @@ $('#saveDetails').click(function(e){
 	try{getOwnedGames();}catch(e){console.log(e);}
 	notifyMe("Details Saved!");
 });
+
 $('#refreshOwned').click(function(e){
 	$(".owned").removeClass("owned");
 	localStorage.removeItem('updatedOwnedApps');
 	localStorage.removeItem('ownedApps');
 	getOwnedGames();
 });
-var bundleApps = [];
-$('.carousel-game-item').each(function(i){
-	var cardsValue = 0;
-	var gameID = $(this).attr('rel');
-	bundleApps.push(gameID);
-	$.ajax({
-		url: "https://api.enhancedsteam.com/market_data/card_prices/?cur=usd&appid="+gameID,
-		success: function(result){
-			if (typeof result !== "object"){
-				return;
-			}
-			$.each(result,function(index,value){
-				if (value.game.indexOf("Foil") <= 0) {
-					cardsValue += Number(value.price);
-				}
-			});
-			var discount = Number((cardsValue/2).toFixed(2));
-			$("[src$='"+gameID+".jpg']").parents(".bundle-item-padding").find('.palette-background-1').append(' ($'+discount.toFixed(2)+")");
-		}
-	});
-});
-function getOwnedGames(){
+
+steamid=false;
+
+if(localStorage.getItem("SteamID") != null && localStorage.getItem("SteamID").length >=1){
+	$("#SteamID").val(localStorage.getItem("SteamID"));
+	steamid=true;
+}else{
+	$('#indiegala-helper h2').addClass("open");
+	$('#indiegala-helper .input-wrapper').slideDown(250);
+}
+
+function getOwnedGames(callback){
 	if(!steamid){
-		$('#indiegala-helper h2').click();
+		$('#indiegala-helper h2').addClass("open");
+		$('#indiegala-helper .input-wrapper').slideDown(250);
 		return;
 	}else if (Number(localStorage.getItem("updatedOwnedApps"))<new Date().getTime()-(86400*1000)){
 		localStorage.removeItem('updatedOwnedApps');
@@ -110,11 +88,10 @@ function getOwnedGames(){
 				localStorage.setItem("ownedApps", JSON.stringify(ownedApps));
 				localStorage.setItem("updatedOwnedApps", new Date().getTime());
 				notifyMe("Owned Games List Updated!");
-				try{showOwnedGames();}catch(e){console.log(e);}
+				try{callback();}catch(e){console.log(e);}
 			}
 		});
 	}else{
-		try{showOwnedGames();}catch(e){console.log(e);}
+		try{callback();}catch(e){console.log(e);}
 	}
 }
-getOwnedGames();

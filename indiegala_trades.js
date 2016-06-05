26b46d8942f3c3b36059a1c541f03d357452714b
RedSparr0w <dan-jmn@hotmail.com>
2016-06-05 19:59:37 +1200

    Add support for Trades - Non guaranteed change BG

---
+++ indiegala_trades.js
@@ -0,0 +1,21 @@
+function showOwnedGames(){
+	var apps = JSON.parse(localStorage.getItem("ownedApps"));
+	$.each(bundleApps,function(i,v){
+		if(typeof apps[v] != "undefined"){
+			$("[src*='"+v+"/header.jpg']").parents(".trade-cont").addClass("owned");
+		}
+	});
+}
+
+var bundleApps = [];
+
+$('.trade-cont img[src*=apps]').each(function(i){ 
+	var str = $('.trade-cont img[src*=apps]').eq(i).attr("src");
+	var start = str.indexOf("apps/");
+	var end = str.indexOf("/header.jpg");
+	var gameID = str.substring(start+5,end);
+	bundleApps.push(gameID);
+	$(this).parents(".trade-cont").find(".read-more").html('<i class="fa fa-steam" aria-hidden="true"></i> <a class="viewOnSteam" href="http://store.steampowered.com/app/'+gameID+'" target="_BLANK">View on Steam &rarr;</a>');
+});
+
+getOwnedGames(showOwnedGames);

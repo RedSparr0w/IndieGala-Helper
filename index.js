94b33edfcf30cffd48b938bf167ab8ef8d485349
RedSparr0w <dan-jmn@hotmail.com>
2016-10-03 16:42:18 +1300

    1.6.10
    General
    - When clicking icon open https://www.indiegala.com/profile page in new
    tab

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.tabs.create({"url": "https://www.indiegala.com/"});
+	chrome.tabs.create({"url": "https://www.indiegala.com/profile"});
 });

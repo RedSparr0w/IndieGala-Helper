43a49461dbde8149225a29296dd79871c8f37ee5
RedSparr0w <dan-jmn@hotmail.com>
2017-07-29 17:21:07 +1200

    Bump Version

--- indiegala.js
+++ indiegala.js
@@ -45,7 +45,7 @@ if (localStorage.getItem("version")===null){
 } else if (localStorage.getItem('version') != version){
 	localStorage.setItem('version',version);
 	/* Display notification relaying update */
-	let update_message = `Minor Fixes.\n"Auto Join" should be working again.`;
+	let update_message = `More Fixes!`;
 	if(!notifyMe(update_message + '\n- v'+version, 'IndieGala Helper Updated')){
 		alert('IndieGala Helper Updated\n' + update_message + '\n- v'+version);
 	}

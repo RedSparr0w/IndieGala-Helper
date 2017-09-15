5a07656dba3a6913bc143e1aa52fbf5a18db1719
RedSparr0w <dan-jmn@hotmail.com>
2017-09-15 16:15:56 +1200

    Firefox Fix Version Check

--- index.js
+++ index.js
@@ -6,11 +6,25 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 	chrome.tabs.create({"url": "https://www.indiegala.com/profile"});
 });
 
-if (Number(localStorage.getItem("version").split(".")[0]) <= 3){
-	localStorage.setItem("version",version);
-	
-	chrome.storage.sync.get({blacklist_apps: {}}, function(blacklist) {
-		chrome.storage.local.set(blacklist);
-	});
+function checkVersion(check_version, above = false){
+	let old_version = !!localStorage.getItem("version") ? localStorage.getItem("version").split(".").map(Number) : "0.0.0";
+	check_version = check_version.split(".").map(Number)
+	while (old_version.length < check_version.length) old_version.push(0);
+	while (check_version.length < old_version.length) check_version.push(0);
+	for (var i = 0; i < check_version.length; ++i) {
+    if (old_version[i] == check_version[i])
+			continue;
+    return above ? old_version[i] > check_version[i] : old_version[i] < check_version[i];
+  }
+	return false;
 }
 
+if (checkVersion("4.0.0", false)){
+	localStorage.setItem("version",version);
+
+	chrome.storage.sync.get({blacklist_apps: {}}, function(blacklist) {
+		chrome.storage.local.set(blacklist, function(blacklist) {
+			chrome.storage.sync.remove('blacklist_apps');
+		});
+	});
+}

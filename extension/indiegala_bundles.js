2843ef8aaef6f29ea1f58cfce5c3d56c945a8047
RedSparr0w <admin@redsparr0w.com>
2020-06-16 22:57:48 +1200

    Slowly remove jQuery dependency

--- extension/indiegala_bundles.js
+++ extension/indiegala_bundles.js
@@ -4,50 +4,42 @@
 function showOwnedApps(){
 	const bundleApps = [];
 
-	// Once games loaded to page get app ids
-	$('.bundle-page-tier-item-col').each(function(){
+	[...document.getElementsByClassName('bundle-page-tier-item-col')].forEach((el)=>{
 		// Check if this app has trading cards
 		// if (/no\s*trading\s*cards/i.test($('.bundle-page-tier-item-trading', this).eq(0).text())) continue;
 
 		let app_id = 0;
-		try { app_id = +$('img', this)[0].src.match(/\/(\d+)\.jpg/)[1]; }catch(O_o){ app_id = 0; }
-
-		// Couldn't determine app_id try the next one
-		if (!app_id) return;
+		try {
+			app_id = +el.getElementsByTagName('img')[0].src.match(/\/(\d+)\.jpg/)[1];
+		}catch(O_o){
+			return;
+		}
 
 		// Mark games as owned
 		if(local_settings.owned_apps.includes(app_id)){
-			$(this).addClass('owned');
+			el.classList.add('owned');
 		}
 
 		bundleApps.push(app_id);
 	});
 
-	// Get card values
-	$.ajax({
-		cache: false,
-		url: `https://indiegala.redsparr0w.com/steamAPI/getCardPrices?apps=${bundleApps.join(',')}`,
-		success: function(result){
-			if (typeof result !== 'object'){
-				return;
+	// Get card values - We don't mind if this data was cached
+	fetch(`https://indiegala.redsparr0w.com/steamAPI/getCardPrices?apps=${bundleApps.join(',')}`).then(res => res.json()).then(result => {
+		bundleApps.forEach(app_id => {
+			// Divide the total by 2 as you get half the total cards as free drops
+			if (result[app_id]){
+				try { document.querySelector(`[src*='${app_id}.jpg']`).closest('.bundle-page-tier-item-col').getElementsByClassName('bundle-page-tier-item-trading')[0].append(` [$${(Math.floor(result[app_id] / 2) / 100).toFixed(2)}]`); }catch(O_o){ /* DO NOTHING */ }
 			}
-
-			bundleApps.forEach(app_id => {
-				// Divide the total by 2 as you get half the total cards as free drops
-				if (result[app_id]){
-					$(`[src*='${app_id}.jpg']`).parents('.bundle-page-tier-item-col').find('.bundle-page-tier-item-trading').append(` [$${(Math.floor(result[app_id] / 2) / 100).toFixed(2)}]`);
-				}
-			});
-		}
-	});
+		});
+	}).catch((O_o)=>{ /* DO NOTHING */ });
 }
 
 // Wait until indiegala loads the initial bundle page
 const wait_for_page = setInterval(() => {
-	if($('.bundle-page-tier-item-col').length >= 1){
+	if(document.getElementsByClassName('bundle-page-tier-item-col').length){
+		// Page loaded
 		clearInterval(wait_for_page);
-
 		// Show owned apps
 		showOwnedApps();
 	}
-}, 500);
+}, 250);
